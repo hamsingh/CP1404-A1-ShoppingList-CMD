bf11855cfac1e5dde5d7f1c85786373f188372b8
Harmon Singh <harmon.singh@my.jcu.edu.au>
2016-08-31 00:57:26 +1000

    Assignment
    Started assignment and read tasksheet to understand requirments.

---
+++ Assignment/A1_V1.py
@@ -0,0 +1,6 @@
+MENU = "R - List required items\nC - List completed items\nA - Add new item\nM - Mark an item as completed\nQ (for quit)"
+print(MENU)
+choice = input(">>> ").upper()
+
+def R ():
+    print("Hello World")
